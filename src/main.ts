d016d297fe72aa03f646d454da86ecbb86d392e5
Maciej Pędzich <dnalop.me@gmail.com>
2022-01-26 00:23:15 +0100

    Import Ace and base splitpanes CSS in Vue app

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import App from './App.vue';
 
+import 'ace-builds/src-min-noconflict/ace';
+import 'ace-builds/src-min-noconflict/theme-tomorrow';
+import 'splitpanes/dist/splitpanes.css';
+
 const app = createApp(App);
 
 app.use(createPinia());
