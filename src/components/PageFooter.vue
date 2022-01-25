c748978f9a0ad04e493bfc638db3a5128340270d
Maciej Pędzich <dnalop.me@gmail.com>
2022-01-26 00:24:05 +0100

    Create a page footer component

---
+++ src/components/PageFooter.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const currentYear = new Date().getFullYear();
+</script>
+
+<template>
+  <footer>
+    <p>&copy; {{ currentYear }} Weekly OverVue. Released under MIT License</p>
+  </footer>
+</template>
+
+<style scoped>
+footer {
+  background-color: #42b883;
+  color: #000;
+  text-align: center;
+}
+</style>
