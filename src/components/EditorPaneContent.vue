1894cfcd6623e70d48996fb1b78cd34b49217336
Maciej Pędzich <dnalop.me@gmail.com>
2022-01-26 00:26:48 +0100

    Create custom pane content wrappers

---
+++ src/components/EditorPaneContent.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+import { CodeEditorLanguage } from '@/types/CodeEditorLanguage';
+
+import CodeEditor from './CodeEditor.vue';
+
+const props = defineProps<{ editorLang: CodeEditorLanguage }>();
+
+const langToLabelMap = {
+  html: 'HTML',
+  css: 'CSS',
+  javascript: 'JavaScript'
+};
+const humanisedLabelText = langToLabelMap[props.editorLang];
+const langLabelClass = `lang-${props.editorLang}`;
+</script>
+
+<template>
+  <div :class="['editor-label', langLabelClass]">
+    <span>{{ humanisedLabelText }}</span>
+  </div>
+  <div class="editor-container">
+    <Suspense>
+      <template #default>
+        <CodeEditor :lang="props.editorLang" />
+      </template>
+      <template #fallback>
+        <span>Loading...</span>
+      </template>
+    </Suspense>
+  </div>
+</template>
+
+<style scoped>
+.editor-container {
+  flex: 1 1 auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.editor-label {
+  padding: 4px 0;
+  text-align: center;
+}
+
+.lang-html,
+.lang-css {
+  color: #fff;
+}
+
+.lang-html {
+  background-color: #e54d26;
+}
+
+.lang-css {
+  background-color: #3799d6;
+}
+
+.lang-javascript {
+  background-color: #f7df1e;
+  color: #000;
+}
+</style>
---
+++ src/components/ResultsPaneContent.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { useCodeStore } from '@/stores/code';
+
+const codeStore = useCodeStore();
+</script>
+
+<template>
+  <span v-if="codeStore.isEmptyDocument">Results will appear here</span>
+  <iframe
+    v-else
+    id="resulting-document"
+    frameborder="0"
+    :srcdoc="codeStore.resultingDocument"
+  ></iframe>
+</template>
+
+<style scoped>
+#resulting-document {
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+}
+</style>
