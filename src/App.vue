829d8d9de5544bab5879f7729554c23a89890235
Maciej Pędzich <dnalop.me@gmail.com>
2022-01-26 00:27:25 +0100

    Modify pane styles and use all components together

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,70 @@
+<script setup lang="ts">
+import { Splitpanes, Pane } from 'splitpanes';
+import EditorPaneContent from './components/EditorPaneContent.vue';
+import ResultsPaneContent from './components/ResultsPaneContent.vue';
+import PageFooter from './components/PageFooter.vue';
+</script>
+
 <template>
-  <h1>Ace Editor Codepen Clone</h1>
+  <Splitpanes id="main-splitpanes" horizontal>
+    <Pane>
+      <Splitpanes id="editor-splitpanes">
+        <Pane>
+          <EditorPaneContent editor-lang="html" />
+        </Pane>
+        <Pane>
+          <EditorPaneContent editor-lang="css" />
+        </Pane>
+        <Pane>
+          <EditorPaneContent editor-lang="javascript" />
+        </Pane>
+      </Splitpanes>
+    </Pane>
+    <Pane id="results-pane">
+      <ResultsPaneContent />
+    </Pane>
+  </Splitpanes>
+  <PageFooter />
 </template>
+
+<style>
+html,
+body,
+#app {
+  width: 100vw;
+  height: 100vh;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+  font-family: Arial, Helvetica, sans-serif;
+  overflow: hidden;
+}
+
+#app,
+.splitpanes__pane {
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+}
+
+#main-splitpanes > .splitpanes__splitter,
+#editor-splitpanes > .splitpanes__splitter {
+  background-color: #35495e;
+}
+
+#main-splitpanes > .splitpanes__splitter {
+  min-height: 3px;
+}
+
+#editor-splitpanes > .splitpanes__splitter {
+  min-width: 3px;
+}
+
+#results-pane {
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+}
+</style>
